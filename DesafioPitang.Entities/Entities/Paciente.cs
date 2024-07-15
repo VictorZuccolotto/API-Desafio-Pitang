bab8928e54bd90e0d143dc27aaaf56470facde5e
Victor Zuccolotto <victor.zuccolotto@ufu.br>
2024-07-14 21:43:46 -0300

    #3 Implementação das entidades

---
+++ DesafioPitang.Entities/Entities/Paciente.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPitang.Entities.Entities
+{
+    public class Paciente: IdEntity<int>
+    {
+
+        public string Nome { get; set; }
+        public DateTime DataNascimento { get; set; }
+        public DateTime DataCriacao { get; set; }
+
+    }
+}
