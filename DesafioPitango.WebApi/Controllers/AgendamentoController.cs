824351e14aecd0f2b49fac83fe25e56bc69410f3
Victor Zuccolotto <victor.zuccolotto@ufu.br>
2024-07-15 16:21:37 -0300

    #6 implementação de controllers

---
+++ DesafioPitango.WebApi/Controllers/AgendamentoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace DesafioPitang.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AgendamentoController : ControllerBase
+    {
+        [HttpGet]
+        public void Get()
+        {
+        }
+
+        [HttpGet("{id}")]
+        public void Get(int id)
+        {
+        }
+
+        [HttpPost]
+        public void Post([FromBody] string agendamento)
+        {
+        }
+
+    }
+}
